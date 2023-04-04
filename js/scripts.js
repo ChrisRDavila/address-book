eee62311ecc049d23ee74bdb7a65556b2acc5bcb
ChrisRDavila <crossdavila@gmail.com>
2023-04-03 18:32:01 -0700

    code updated, functional

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,15 @@ function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
 
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
 };
 
 AddressBook.prototype.findContact = function(id) {
@@ -19,13 +20,14 @@ AddressBook.prototype.findContact = function(id) {
   }
   return false;
 };
+
 AddressBook.prototype.deleteContact = function(id) {
   if (this.contacts[id] === undefined) {
     return false;
   }
   delete this.contacts[id];
   return true;
-};//
+};
 
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
@@ -38,9 +40,31 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
-// User Interface Logic
-
+// User Interface Logic ---------
 let addressBook = new AddressBook();
+
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
+function displayContactDetails(event) {
+  const contact = addressBook.findContact(event.target.id);
+  document.querySelector(".first-name").innerText = contact.firstName;
+  document.querySelector(".last-name").innerText = contact.lastName;
+  document.querySelector(".phone-number").innerText = contact.phoneNumber;
+  document.querySelector("button.delete").setAttribute("id", contact.id);
+  document.querySelector("div#contact-details").removeAttribute("class");
+}
 
 function handleFormSubmission(event) {
   event.preventDefault();
@@ -49,9 +73,23 @@ function handleFormSubmission(event) {
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
   let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
   addressBook.addContact(newContact);
-  console.log(addressBook.contacts);
+  listContacts(addressBook);
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
 }
+}
+
+function handleDelete(event) {
+  addressBook.deleteContact(event.target.id);
+  document.querySelector("button.delete").removeAttribute("id");
+  document.querySelector("div#contact-details").setAttribute("class", "hidden");
+  listContacts(addressBook);
+}
+
 
 window.addEventListener("load", function (){
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
 });
